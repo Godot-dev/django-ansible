38515190d29b3450feac2c2e67999b913dfc4a49
Eden PORTE <edenporte@gmail.com>
2025-04-14 15:40:38 +0200

    reorga ansible

--- mysite/app1/views.py
+++ mysite/app1/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app1.models import *
 
 
 # Create your views here.
-def hello(request):
+def index(request):
     context = {"personnes": Person.objects.all()}
     return render(request, 'app1/index.html', context)
 
 def submit_form(request):
     p = Person.objects.create(name=request.POST.get('name'), age=request.POST.get('age'))
     p.save()
-    return hello(request)
+    return redirect("/")
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,6 @@ from app1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
+    path('', views.index),
     path('submit/', views.submit_form)
 ]
